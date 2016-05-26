1db7a78ab0d08e4b3287ece38281757a6bc89c29
mikhail.kopylov <mikhail.kopylov@sperasoft.com>
2016-05-26 11:49:44 +0300

    Complete PikabuService

--- src/main/java/ru/omickron/rest/PikabuFeed.java
+++ src/main/java/ru/omickron/rest/PikabuFeed.java
@@ -1,9 +1,10 @@
 package ru.omickron.rest;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MediaType;
 
+import com.google.common.base.Charsets;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,9 @@ import ru.omickron.model.Rss;
 import ru.omickron.service.PikabuService;
 
 @RestController
-@RequestMapping(value = "/pikabu")
+@RequestMapping(value = PikabuFeed.URL)
 public class PikabuFeed {
+    public static final String URL = "/pikabu";
     private final PikabuService service;
 
     @Autowired
@@ -20,8 +22,8 @@ public class PikabuFeed {
         this.service = service;
     }
 
-    @RequestMapping(value = "/{type:.*}/{id:.*}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_XML})
-    public Rss getChannel( @PathVariable("type") String type, @PathVariable("id") String id ) {
-        return service.getRss( type, id );
-    }
+    @RequestMapping(value = "/**", method = RequestMethod.GET, produces = {MediaType.APPLICATION_XML})
+    public Rss getChannel( HttpServletRequest request ) {
+        String servletPath = new String(request.getServletPath().getBytes(Charsets.ISO_8859_1), Charsets.UTF_8);
+        return service.getRss( servletPath.substring( URL.length() ) );    }
 }
--- src/main/java/ru/omickron/service/PikabuService.java
+++ src/main/java/ru/omickron/service/PikabuService.java
@@ -1,35 +1,36 @@
 package ru.omickron.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.google.common.base.Charsets;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriUtils;
 import ru.omickron.model.Rss;
 import ru.omickron.model.RssChannel;
 import ru.omickron.model.RssItem;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 import static org.apache.commons.lang.Validate.isTrue;
 
 @Component
 @Slf4j
 public class PikabuService extends AbstractLoadingService {
-    private static final String URL = "http://pikabu.ru/";
-
     @Cacheable(value = "rss")
-    public Rss getRss( String type, String id ) {
-        log.debug( "Generating channel {} {}", type, id );
-        return new Rss( getChannel( type, id ) );
+    public Rss getRss( String id ) {
+        log.debug( "Generating channel {}", id );
+        return new Rss( getChannel( id ) );
     }
 
-    private RssChannel getChannel( String type, String id ) {
-        String page = getPageHtml( type, id );
+    private RssChannel getChannel( String id ) {
+        String page = getPageHtml( id );
         Document document = Jsoup.parse( page );
         RssChannel channel;
         Elements profileInfo = document.select( ".profile_wrap" );
@@ -49,22 +50,22 @@ public class PikabuService extends AbstractLoadingService {
     }
 
     private List<RssItem> getItems( Elements stories ) {
-        return stories.select( ".story" ).stream().map( o -> {
-            Elements titleElements = o.select( ".story__header-title a" );
-            String title = titleElements.html();
-            String link = titleElements.attr( "href" );
-            String pubDateString = o.select( ".story__date" ).attr( "title" );
-            DateTime pubDate = DateTime.parse( pubDateString, DateTimeFormat.forPattern( "dd MMMM YYYY в HH:mm" ) );
-            String description = o.select( ".b-story__content" ).html();
-            return new RssItem( title, link, description, pubDate.getMillis() );
-        } ).collect( Collectors.toList() );
+        return stories.select( ".story" ).stream()
+                .filter( o -> isNotBlank( o.select( ".story__date" ).attr( "title" ) ) ).map( o -> {
+                    Elements titleElements = o.select( ".story__header-title a" );
+                    String title = titleElements.html();
+                    String link = titleElements.attr( "href" );
+                    String pubDateString = o.select( ".story__date" ).attr( "title" );
+                    String description = o.select( ".b-story__content" ).html();
+                    return new RssItem( title, link, description, Long.parseLong( pubDateString ) );
+                } ).collect( Collectors.toList() );
     }
 
     private RssChannel getChannelFromSearch( Elements searchInfo ) {
-        Elements tagElements = searchInfo.select( ".search-tags-container .tb-tag-complete" );
+        Elements tagElements = searchInfo.select( ".search-tags-container .search-startup-tag :first-child" );
         isTrue( !tagElements.isEmpty() );
         String tagsPart = tagElements.size() > 1 ? "тегами" : "тегом";
-        String tagsString = tagElements.stream().map( o -> o.attr( "data-tag" ) ).collect( Collectors.joining( ", " ) );
+        String tagsString = tagElements.stream().map( o -> o.html() ).collect( Collectors.joining( ", " ) );
         String channelName = String.format( "Записи с %s %s", tagsPart, tagsString );
         return new RssChannel( channelName, String.format( "http://pikabu.ru/tag/%s", tagsString ), channelName );
     }
@@ -78,7 +79,12 @@ public class PikabuService extends AbstractLoadingService {
                 String.format( "Все посты пользователя %s", name ) );
     }
 
-    private String getPageHtml( String type, String id ) {
-        return client.resource( URL + type + "/" + id ).get( String.class );
+    private String getPageHtml( String id ) {
+        try {
+            return client.resource( "http://pikabu.ru" + UriUtils.encodePath( id, Charsets.UTF_8.name() ) )
+                    .get( String.class );
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException( e );
+        }
     }
 }
